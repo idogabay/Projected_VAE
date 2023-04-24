b78d33227b9c9d1441016d08a33b6f9559b165fe
ido <idogabay@campus.technion.ac.il>
2023-04-24 14:10:19 +0300

    update images size

--- resize_pics.py
+++ resize_pics.py
@@ -33,9 +33,9 @@ def resize_folder(pics_root_dir,resized_pics_dir,width = 224,height = 224):
             new_image.save(new_image_path)
 # resize all the pics in the folder
 def main():
-    base_path = "/home/ido/datasets/projected_vae/anime-face"
-    pics_root_dir = os.path.join(base_path,"img") #src_folder
-    pics_new_root_dir= os.path.join(base_path,"resized_images") #dst_folder
+    base_path = "/home/ido/datasets"
+    pics_root_dir = os.path.join(base_path,"blah") #src_folder
+    pics_new_root_dir= os.path.join(base_path,"blah2") #dst_folder
     #xml_root_folder = "/home/picky/Documents/datasets/exp_date/30.8.22/labels (copy)" #labels_folder
     width = 256
     height = 256
